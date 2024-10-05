7d0eb19cd3d6c7eb98e2653898fb2e59d0c07369
eyedeekay <hankhill19580@gmail.com>
2024-10-04 21:01:25 -0400

    scripts to discover IDs for use with _locales

--- contents.sh
+++ contents.sh
@@ -1,2 +1,12 @@
 #! /usr/bin/env bash
+echo "function contentUpdateById(id, message) {
+  let infoTitle = document.getElementById(id);
+  let messageContent = chrome.i18n.getMessage(message);
+  if (infoTitle === null) {
+    console.log(\"content error\", id, messageContent);
+    return;
+  }
+  infoTitle.textContent = messageContent;
+}"
+
 for f in $(find-ids $1); do echo  -n "contentUpdateById(\"$f\", \""; lowercase $(capitalize $f | tr -d '-'); echo "\");"; done
